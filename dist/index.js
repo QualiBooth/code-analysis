f3472f64cfd477de54a9baa037c6657a4bd10fcf
rvitanov <289965785+rvitanov@users.noreply.github.com>
2026-06-02 10:12:42 +0000

    chore: rebuild dist

--- dist/index.js
+++ dist/index.js
@@ -192768,7 +192768,7 @@ async function runEslint(projectType, scanPaths, repoRoot) {
 
   const eslint = new ESLint({
     cwd: repoRoot,
-    overrideConfigFile: false,
+    overrideConfigFile: true,
     overrideConfig,
     errorOnUnmatchedPattern: false,
   })
